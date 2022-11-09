bfe9892c752d43be8539cafbdbe5a122f2ea1609
JazykB <77745459+JazykB@users.noreply.github.com>
2022-11-09 20:08:10 +0800

    Create CartRemoveProductButton.php

---
+++ CartRemoveProductButton.php
@@ -0,0 +1,6 @@
+<!-- 
+    This button/link is on every product on the cart view
+ -->
+
+<!-- change the cart.php part to the url of viewing cart page -->
+<a href="cart.php&remove=<?=$product['id']?>" class="remove">Remove</a>
