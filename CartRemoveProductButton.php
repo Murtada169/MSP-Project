8199f7f9ef2407320d2f13e2513953fb1b10c340
JazykB <77745459+JazykB@users.noreply.github.com>
2022-11-09 20:13:50 +0800

    Update CartRemoveProductButton.php

--- CartRemoveProductButton.php
+++ CartRemoveProductButton.php
@@ -1,6 +1,11 @@
 <!-- 
     This button/link is on every product on the cart view
+    It will redirect/refresh to the cart view again with added product id to be removed
+    using GET, the product id will be accessed and the product will be removed
  -->
 
-<!-- change the cart.php part to the url of viewing cart page -->
+<!--
+    Change the cart.php part to the url of viewing cart page 
+    Copy the content to the view cart page, on the part where each product name/price is displayed
+-->
 <a href="cart.php&remove=<?=$product['id']?>" class="remove">Remove</a>
