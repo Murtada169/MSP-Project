f69d9a2ecd0da33d872169ee435b33393f14596d
JazykB <77745459+JazykB@users.noreply.github.com>
2022-11-09 20:07:57 +0800

    Added comment on how to use this element

--- CartAddProductButton.php
+++ CartAddProductButton.php
@@ -4,7 +4,11 @@
     $product['id'] will be passed using POST method to CartAddProductLogic.php
  -->
 
-<form action="index.php?page=cart" method="post">
+<!-- 
+    Change the cart.php to the url of the viewing cart page 
+    Copy the content of this script to the product page
+-->
+<form action="cart.php" method="post">
     <input type="number" name="quantity" value="1" min="1" placeholder="Quantity" required>
     <input type="hidden" name="product_id" value="<?=$product['id']?>">
     <input type="submit" value="Add To Cart">
