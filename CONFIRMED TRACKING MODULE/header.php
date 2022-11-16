65e424b615e0d515a148bf12e6d6be4d2fcb7ffc
mrnymj <115123310+mrnymj@users.noreply.github.com>
2022-11-16 16:35:48 +0800

    confirmed tracking module

---
+++ CONFIRMED TRACKING MODULE/header.php
@@ -0,0 +1,81 @@
+<link href="style.css" rel="stylesheet" />
+  <!-- To get icons in navigation bar -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css"
+  />
+  <!-- Font links -->
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Faustina:wght@300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Ephesis&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Beau+Rivage&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap"
+    rel="stylesheet"
+  />
+
+
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@300&display=swap" rel="stylesheet">
+    <head>
+      <title>Cacti-Succulent Kuching</title>
+    </head>
+ 
+      <header>
+        <a href="home.php" id="logo"> <h1>Cacti-Succulent Kuching</h1></a>
+        <nav>
+          <ul id="ulNavBar">
+            <li class="navigationBar">
+              <a href="#contact"
+                ><i class="fa fa-address-book" aria-hidden="true"></i>Contact
+                Us</a
+              >
+            </li>
+            <li class="navigationBar">
+              <a href="#login"
+                ><i class="fa fa-sign-in" aria-hidden="true"></i>Log in</a
+              >
+            </li>
+            <li class="navigationBar">
+              <a href="comingSoon.php"
+                ><i class="fa fa-info" aria-hidden="true"></i>Catalogue</a
+              >
+            </li>
+            <li class="navigationBar">
+              <a href="#booking"
+                ><i class="fa fa-calendar" aria-hidden="true"></i>Booking</a
+              >
+            </li>
+            <li class="navigationBar">
+              <a class="active" href="home.php"
+                ><i class="fa fa-fw fa-home" aria-hidden="true"></i>Home</a
+              >
+            </li>
+          </ul>
+        </nav>
+      </header>
